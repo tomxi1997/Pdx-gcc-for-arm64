085b77fbda999ac7588674466f1ba01d47bf82af
tomxi1997 <78306449+tomxi1997@users.noreply.github.com>
2023-09-26 23:27:29 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,8 @@
+ #!/usr/bin/env bash
+rm -rf ../pdx-gcc-*
+./build-gcc-64.sh -a arm64
+./build-gcc-32.sh -a arm
+cd ..
+XZ_OPT="-9" tar --warning=no-file-changed -cJf pdx-gcc-`date +'%Y%m%d'`.tar.xz pdx-gcc-`date +'%Y%m%d'`
+echo "Compression packaging GCC!"
+
