8102e499ad6d6fb36c69bafdde483c365e920602
tomxi1997 <78306449+tomxi1997@users.noreply.github.com>
2023-10-30 11:36:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf ../pdx-gcc-*
 ./build-gcc-64.sh -a arm64
 ./build-gcc-32.sh -a arm
 cd ..
-tar -I pixz -cf pdx-gcc-`date +'%Y%m%d'`.tar.xz pdx-gcc-`date +'%Y%m%d'`
-#XZ_OPT="-9" tar --warning=no-file-changed -cJf pdx-gcc-`date +'%Y%m%d'`.tar.xz pdx-gcc-`date +'%Y%m%d'`
+#tar -I pixz -cf pdx-gcc-`date +'%Y%m%d'`.tar.xz pdx-gcc-`date +'%Y%m%d'`
+XZ_OPT="-9" tar --warning=no-file-changed -cJf pdx-gcc-`date +'%Y%m%d'`.tar.xz pdx-gcc-`date +'%Y%m%d'`
 echo "Compression packaging GCC!"
 
